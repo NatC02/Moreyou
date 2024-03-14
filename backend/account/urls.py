0baa0c41f6927827336cdcf4893fb1d936a28c49
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-13 20:07:55 -0400

    Add login API endpoint using JWT authentication

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', api.signup, name='signup'),
+    path('signup/', api.signup, name='signup'),
     path('me/', api.me, name="me"),
-    path('friends/request/<uuid:pk>', api.send_friend_request, name='send_friend_request'),
-    
 ]
