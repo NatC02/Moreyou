088ff653447cd427e84bd142c9ca51cb82da25b2
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-14 23:48:50 -0400

    Implement profile API endpoint for post retrieval

--- backend/post/api.py
+++ backend/post/api.py
@@ -13,6 +13,14 @@ def post_list(request):
 
     return JsonResponse(serializer.data, safe=False)
 
+@api_view(['GET'])
+def post_list_profile(request, id):
+    posts = Post.objects.filter(created_by_id=id)
+
+    serializer = PostSerializer(posts, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
 @api_view(['POST'])
 def post_create(request):
 
@@ -22,12 +30,8 @@ def post_create(request):
         post = form.save(commit=False)
         post.created_by = request.user
         post.save()
-
         serializer = PostSerializer(post)
         return JsonResponse(serializer.data, safe=False)
 
     else:
         return JsonResponse({'error': "Error goes here ... "})
-
-
-
