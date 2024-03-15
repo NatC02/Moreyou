8a97fed1bfb2c46900eeb5f25e802f307fe13d9f
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-14 23:24:44 -0400

    Implement post creation API endpoint

--- backend/post/api.py
+++ backend/post/api.py
@@ -3,12 +3,31 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from django.shortcuts import render
 from . serializers import PostSerializer
 from . models import Post
+from . forms import PostForm
 
 # Create your views here.
 @api_view(['GET'])
 def post_list(request):
     posts = Post.objects.all()
-
     serializer = PostSerializer(posts, many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+@api_view(['POST'])
+def post_create(request):
+
+    form = PostForm(request.data)
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data, safe=False)
+
+    else:
+        return JsonResponse({'error': "Error goes here ... "})
+
+
+
