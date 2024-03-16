6af5952ab0e3a740875eafd4d1d42194e5ca0202
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-15 20:45:12 -0400

    Introduce search functionality backend object url config

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('api/account/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('api/posts/', include('post.urls')),
+    path('api/search/', include('search.urls') ),
 ]
