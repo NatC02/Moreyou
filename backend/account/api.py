d14a3f2d999222c53be4407198938ba56bf1a354
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-15 22:03:07 -0400

    Introduce API endpoint for sending friend requests

--- backend/account/api.py
+++ backend/account/api.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from . forms import SignupForm
+from . models import FriendshipRequest
+from . models import User
+from .serializers import UserSerializer
+
 
 @api_view(['POST'])
 @authentication_classes([])
@@ -8,22 +12,17 @@ from . forms import SignupForm
 def signup(request):
     data = request.data 
     message = 'success'
-
     form = SignupForm({
         'email': data.get('email'),
         'name': data.get('name'),
         'password1': data.get('password1'),
         'password2': data.get('password2')
-
     })
-
     if form.is_valid():
         form.save()
     else:
         message = 'error'
-
     return JsonResponse({'message': message})
-
 @api_view(['GET'])
 def me(request):
     return JsonResponse({
@@ -31,3 +30,12 @@ def me(request):
         'name': request.user.name,
         'email': request.user.email,
     })
+
+@api_view(['POST'])
+def send_friend_request(request, pk):
+    user = User.objects.get(pk=pk)
+
+    friendship_request = FriendshipRequest(created_for=user, created_by=request.user)
+
+
+    return JsonResponse({'message': "Friendship request created"})
